51e9869225057c46b4d51326d77315a84995716d
1XDAVIDX <davidgelvez122j@gmail.com>
2023-10-23 11:17:33 -0500

    Proy_12

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
+// Importando módulos necesarios
 const readline = require('readline');
 const fs = require('fs');
-require('colors');
+require('colors'); // Agrega colores a la salida en consola
 
+// Creando una interfaz para leer y escribir en consola
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+// Definiendo una clase para representar un Producto
 class Producto {
+  // Propiedades privadas del Producto
   #codigoProducto;
   #nombreProducto;
   #inventarioProducto;
   #precioProducto;
 
+  // Constructor del Producto
   constructor(){
     this.#codigoProducto = '';
     this.#inventarioProducto = '';
@@ -20,24 +25,31 @@ class Producto {
     this.#precioProducto = 0;
   }
 
+  // Métodos para establecer y obtener el código del producto
   set setCodigoProducto(value){
     this.#codigoProducto = value;
   }
   get getCodigoProducto(){
     return this.#codigoProducto;
   }
+
+  // Métodos para establecer y obtener el inventario del producto
   set setInventarioProducto(value){
     this.#inventarioProducto = value;
   }
   get getInventarioProducto(){
     return this.#inventarioProducto;
   }
+
+  // Métodos para establecer y obtener el nombre del producto
   set setNombreProducto(value){
     this.#nombreProducto = value;
   }
   get getNombreProducto(){
     return this.#nombreProducto;
   }
+
+  // Métodos para establecer y obtener el precio del producto
   set setPrecioProducto(value){
     this.#precioProducto = value;
   }
@@ -46,32 +58,35 @@ class Producto {
   }
 }
 
+// Definiendo una clase para representar una Tienda de Productos
 class ProductosTienda {
+  // Propiedad privada: lista de productos
   #listaProducto;
 
   constructor(){
     this.#listaProducto = [];
   }
 
+  // Método para obtener la lista de productos
   get getListaProductos(){
     return this.#listaProducto;
   }
 
-  
-
+  // Método para agregar un producto a la lista y guardar en archivo
   agregarProducto(producto) {
     this.#listaProducto.push(producto);
-    this.grabarArchivoProductos();
+    this.grabarArchivoProductos(); // Guardar en archivo
   }
 
+  // Método para cargar productos desde un archivo
   cargarArchivosProductos() {
     let datosArchivo = [];
 
     try {
-      const data = fs.readFileSync('datos.json', 'utf8');
-      datosArchivo = JSON.parse(data);
+      const data = fs.readFileSync('datos.json', 'utf8'); // Leer archivo
+      datosArchivo = JSON.parse(data); // Convertir a objeto JavaScript
     } catch (error) {
-      console.error(error);
+      console.error(error); // Manejar error en caso de fallo
     }
 
     let contador = 0;
@@ -91,6 +106,7 @@ class ProductosTienda {
     console.log(`Total de productos cargados`.bgRed,` ==>`.bgYellow,` ${contador}`.bgRed);
   }
 
+  // Método para guardar productos en un archivo
   grabarArchivoProductos() {
     const instanciaClaseAObjetos = this.getListaProductos.map(producto =>{
       return {
@@ -104,49 +120,52 @@ class ProductosTienda {
     const cadenaJson = JSON.stringify(instanciaClaseAObjetos,null,2);
     const nombreArchivo = 'datos.json';
 
-    fs.writeFileSync(nombreArchivo, cadenaJson, 'UTF-8');
+    fs.writeFileSync(nombreArchivo, cadenaJson, 'UTF-8'); // Escribir en archivo
     console.log(`DATOS GUARDADOS EN ${nombreArchivo}`);
   }
 
+  // Método para mostrar todos los productos en consola
   mostrarProductos() {
     this.getListaProductos.forEach(producto => {
       console.log(`│ Código: ${producto.getCodigoProducto} │ Nombre: ${producto.getNombreProducto} │ Inventario: ${producto.getInventarioProducto} │ Precio: ${producto.getPrecioProducto} │`);
     });
   }
-
-  
 }
 
+// Crear una instancia de la clase ProductosTienda
 let productosTienda = new ProductosTienda;
 
+// Función para agregar un producto desde la consola
 function agregarProductoDesdeConsola() {
   rl.question('Ingrese el código del producto: ', (codigo) => {
     rl.question('Ingrese el nombre del producto: ', (nombre) => {
       rl.question('Ingrese el inventario del producto: ', (inventario) => {
         rl.question('Ingrese el precio del producto: ', (precio) => {
-          const nuevoProducto = new Producto();
-          nuevoProducto.setCodigoProducto = codigo;
+          const nuevoProducto = new Producto(); // Crear un nuevo producto
+          nuevoProducto.setCodigoProducto = codigo; // Establecer propiedades
           nuevoProducto.setNombreProducto = nombre;
           nuevoProducto.setInventarioProducto = inventario;
           nuevoProducto.setPrecioProducto = precio;
 
-          productosTienda.agregarProducto(nuevoProducto);
+          productosTienda.agregarProducto(nuevoProducto); // Agregar producto a la tienda
 
-          console.clear(); 
+          console.clear(); // Limpiar consola
           console.log('Producto agregado exitosamente.'.bgGreen);
 
-          mostrarMenu(); 
+          mostrarMenu(); // Mostrar el menú nuevamente
         });
       });
     });
   });
 }
 
+// Función para mostrar todos los productos en consola
 function mostrarProductosDesdeConsola() {
   productosTienda.mostrarProductos();
-  mostrarMenu(); 
+  mostrarMenu(); // Mostrar el menú nuevamente
 }
 
+// Función para mostrar el menú de opciones en consola
 function mostrarMenu() {
   console.log('******************************************************************'.blue);
   console.log(`***********************`.blue,  `INVENTÁRIO`.bgGreen,`  *****************************`.blue);
@@ -155,25 +174,26 @@ function mostrarMenu() {
   rl.question(`1. Agregar Producto\n2. Mostrar Productos\n3. Limpiar Consola\n4. Salir\nSeleccione una opción:\n` , (opcion) => {
     switch (opcion) {
       case '1':
-        agregarProductoDesdeConsola();
+        agregarProductoDesdeConsola(); // Llamar función para agregar producto
         break;
       case '2':
-        mostrarProductosDesdeConsola();
+        mostrarProductosDesdeConsola(); // Llamar función para mostrar productos
         break;
       case '3':
-        Limpiar();
+        Limpiar(); // Llamar función para limpiar la consola
         break;
       case '4':
-        rl.close();
+        rl.close(); // Cerrar la interfaz de consola
         break;
       default:
         console.log('Opción no válida. Por favor, seleccione una opción válida.'.bgRed);
-        mostrarMenu();
+        mostrarMenu(); // Mostrar el menú nuevamente
         break;
     }
   });
 }
 
+// Función para limpiar la consola
 function Limpiar() {
   console.clear();
   console.log(`Total de productos cargados`.bgRed,` ==>`.bgYellow,` ${productosTienda.getListaProductos.length}`.bgRed);
@@ -181,14 +201,16 @@ function Limpiar() {
   
 }
 
+// Función principal
 function main() {
   console.clear();
-  productosTienda.cargarArchivosProductos(); 
+  productosTienda.cargarArchivosProductos();
   mostrarMenu();
 }
 
 
 
-main(); 
+// Llamar a la función principal para iniciar el programa
+main();
 
 
